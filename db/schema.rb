8e163191c834ee13f66ac6ab7008c778691bb55f
taisuke-ohmi <taisukeohmi0420@gmail.com>
2018-08-25 23:34:30 +0900

    has_secure_password追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_25_123839) do
+ActiveRecord::Schema.define(version: 2018_08_25_142348) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
