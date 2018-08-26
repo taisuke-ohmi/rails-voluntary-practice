e9113e24e972b0dcc07580e757e2e5460712b577
taisuke-ohmi <taisukeohmi0420@gmail.com>
2018-08-26 14:48:52 +0900

    userをusersに変更

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,4 +1,4 @@
-module UserHelper
+module UsersHelper
   def gravatar_for(user)
     gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
     gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
