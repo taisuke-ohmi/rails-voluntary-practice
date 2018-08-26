3d4ae03f2c6cf952659665b91c1c9c919ed66ab9
taisuke-ohmi <taisukeohmi0420@gmail.com>
2018-08-26 16:18:05 +0900

    sessionsコントローラー追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+  get 'sesstions/new'
   get 'user/new'
   root 'static_pages#home'
   get '/home', to: 'static_pages#home'
   get '/help', to: 'static_pages#help'
   get '/signup', to: 'users#new'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
   resources :users
+  
 end
