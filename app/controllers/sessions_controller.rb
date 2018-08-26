826b3715484b30d84d9ed607b8479b7b0d3a3618
taisuke-ohmi <taisukeohmi0420@gmail.com>
2018-08-26 16:46:39 +0900

    セッション作成

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,19 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    p = params[:session]
+    user = User.find_by(email: p[:email].downcase)
+    if user && user.authenticate(p[:password])
+      log_in(user)
+      redirect_to user
+    else
+      flash[:danger] = "Invalid email/password combination"
+      render 'new'
+    end
+  end
+
+  def destroy
+  end
 end
