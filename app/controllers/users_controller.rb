3b82093c626c0e0f8c04b6e40c72df652953c313
taisuke-ohmi <taisukeohmi0420@gmail.com>
2018-08-28 21:27:16 +0900

    updateアクション追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -22,6 +22,14 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+    else
+      render 'edit'
+    end
+  end
+
   private
     def user_params
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
