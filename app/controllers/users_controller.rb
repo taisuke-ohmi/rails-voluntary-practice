d87c2574ef56874f2e4417e6726c2a67d01a381d
taisuke-ohmi <taisukeohmi0420@gmail.com>
2018-08-28 21:42:05 +0900

    before_action追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:edit, :update]
+
   def show
     @user = User.find(params[:id])
   end
@@ -33,5 +35,12 @@ class UsersController < ApplicationController
   private
     def user_params
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
+
+    def logged_in_user
+      if !logged_in?
+        flash[:danger] = "Please log in."
+        redirect_to login_url
+      end
     end
 end
