1db9edd46b3bbda7bb34267152a46ddc2f5e7016
taisuke-ohmi <taisukeohmi0420@gmail.com>
2018-08-30 23:24:59 +0900

    micropostsテーブルにindex追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_30_141917) do
+ActiveRecord::Schema.define(version: 2018_08_30_142107) do
 
   create_table "microposts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.text "content"
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["user_id", "created_at"], name: "index_microposts_on_user_id_and_created_at"
     t.index ["user_id"], name: "index_microposts_on_user_id"
   end
 
