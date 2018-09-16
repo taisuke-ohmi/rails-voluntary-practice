19de77654a9a98278eae3c1c2b851540c675072f
taisuke-ohmi <taisukeohmi0420@gmail.com>
2018-09-16 10:58:25 +0900

    logged_in_userをApplicationControllerのprivate関数に

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,4 +1,6 @@
 class MicropostsController < ApplicationController
+  before_action :logged_in_user, only: [:create, :destroy]
+
   def show
     @user = User.find(params[:id])
     @microposts = @user.microposts.paginate(page: params[:page])
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -49,14 +49,6 @@ class UsersController < ApplicationController
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
     end
 
-    def logged_in_user
-      if !logged_in?
-        store_location
-        flash[:danger] = "Please log in."
-        redirect_to login_url
-      end
-    end
-
     def correct_user
       @user = User.find(params[:id])
       if !current_user?(@user)
